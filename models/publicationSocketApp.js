766bb4d202e9bff30c5f660c7ca6f81ffdea472e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-11-07 18:28:39 +0000

    Transaction maj site

--- models/publicationSocketApp.js
+++ models/publicationSocketApp.js
@@ -5,36 +5,35 @@ function configurationEvenements(socket) {
   const configurationEvenements = {
     listenersPrives: [
       {eventName: 'publication/requeteSites', callback: (params, cb) => {requeteSites(socket, params, cb)}},
+      {eventName: 'publication/requeteSite', callback: (params, cb) => {requeteSite(socket, params, cb)}},
     ],
     listenersProteges: [
-      // {eventName: 'coupdoeil/ajouterCatalogueApplication', callback: (transaction, cb) => {
-      //   ajouterCatalogueApplication(socket, transaction, cb)
-      // }},
+      {eventName: 'publication/ajouterSite', callback: (transaction, cb) => {
+        ajouterSite(socket, transaction, cb)
+      }},
     ]
   }
 
   return configurationEvenements
 }
 
-// async function ajouterCatalogueApplication(socket, transaction, cb) {
-//   // Ajout d'un catalogue d'application avec transaction preformattee
-//   console.debug("Recu ajouterCatalogueApplication : %O", transaction)
-//   try {
-//     if(transaction['en-tete'].domaine === 'CatalogueApplications.catalogueApplication') {
-//       const amqpdao = socket.amqpdao
-//       const reponse = await amqpdao.transmettreEnveloppeTransaction(transaction)
-//       return cb(reponse)
-//     } else {
-//       // Par defaut
-//       cb({err: 'Mauvais domaine pour ajouterCatalogueApplication : ' + transaction.domaine})
-//     }
-//   } catch(err) {
-//     console.error("ajouterCatalogueApplication %O", err)
-//     cb({err: ''+err})
-//   }
-//
-// }
-//
+async function ajouterSite(socket, transaction, cb) {
+  // Ajout d'un nouveau site
+  console.debug("Recu ajouterSite : %O", transaction)
+  try {
+    if(transaction['en-tete'].domaine === 'Publication.majSite') {
+      const amqpdao = socket.amqpdao
+      const reponse = await amqpdao.transmettreEnveloppeTransaction(transaction)
+      return cb(reponse)
+    } else {
+      return cb({err: 'Mauvais domaine pour ajouterSite : ' + transaction.domaine})
+    }
+  } catch(err) {
+    console.error("ajouterSite %O", err)
+    return cb({err: ''+err})
+  }
+}
+
 // function traiterCommande(rabbitMQ, enveloppe, cb) {
 //   // console.debug("Enveloppe de commande recue");
 //   // console.debug(enveloppe);
@@ -68,6 +67,10 @@ function configurationEvenements(socket) {
 
 function requeteSites(socket, params, cb) {
   executerRequete('Publication.listeSites', socket, params, cb)
+}
+
+function requeteSite(socket, params, cb) {
+  executerRequete('Publication.configurationSite', socket, params, cb)
 }
 
 async function executerRequete(domaineAction, socket, params, cb) {
